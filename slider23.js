b1f93bbbb2e2e9683c70fc56212b5a125f8d1c83
Ronny Neefe <ronny@neefe.org>
2013-09-14 13:27:05 +0200

    Added method to measure the dimensions of the list

--- slider23.js
+++ slider23.js
@@ -31,9 +31,60 @@
     init: function() {
       var self = this;
 
-      /*self.$listElements.each(function() {
-        $
-      });*/
+      self.measureList();
+    },
+
+    measureList: function() {
+      var self = this;
+
+      self.toggleElementForMeasure();
+
+      self.getDimensionListElements(function(dimensions) {
+
+      });
+
+      self.toggleElementForMeasure();
+    },
+
+    toggleElementForMeasure: function() {
+      var self = this;
+
+      if(self.$element.hasClass('visibleForMeasure'))
+        self.$element.removeClass('visibleForMeasure');
+      else
+        self.$element.addClass('visibleForMeasure');
+    },
+
+    getDimensionListElements: function(callback) {
+      var self = this;
+
+      var fullWidth, numOfListElements, maxHeight, dimensions;
+
+      numOfListElements = self.$listElements.length;
+
+      fullWidth = 0;
+      maxHeight = 0;
+      self.$listElements.each(function(index) {
+        var $listElement, listWidth, maxHeight;
+
+        $listElement = $(this);
+
+        listWidth = $listElement.outerWidth(true);
+        listElementHeight = $listElement.outerHeight(true);
+
+        if(listElementHeight > maxHeight)
+          maxHeight = listElementHeight;
+
+        fullWidth += listWidth;
+
+        dimensions = {
+          'maxHeight': maxHeight,
+          'fullWidth': fullWidth
+        };
+
+        if(index+1 == numOfListElements)
+          callback(dimensions);
+      });
     }
   };
 
