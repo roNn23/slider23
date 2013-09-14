5e0aa9e9c1e6d6bcaff28e3174a53c33f714221b
Ronny Neefe <ronny@neefe.org>
2013-09-14 23:25:57 +0200

    Keyboard navigation + animation added

--- slider23.js
+++ slider23.js
@@ -12,17 +12,22 @@
 
     // Defaults
     self.defaults = {
-
+      keyboardNavigation: true,
+      moveDistanceInPx: 1000
     };
 
     // Extending options
     self.opts = $.extend({}, self.defaults, options);
 
     // Privates
-    self.$element = $(el);
+    self.$wrapper = $(el);
     self.$list = $('ul', el);
     self.$listElements = $('li', self.$list);
+
     self.$window = $(window);
+    self.$document =  $(document);
+
+    self.movedX = 0;
   }
 
   // Separate functionality from object creation
@@ -31,39 +36,108 @@
     init: function() {
       var self = this;
 
-      self.measureList();
+      self.bindKeyboard();
+
+      self.enlargeList(function() {
+        self.showSlider();
+      });
     },
 
-    measureList: function() {
+    bindKeyboard: function() {
+      var self = this;
+
+      if(self.opts.keyboardNavigation == false)
+        return;
+
+      self.$document.unbind("keydown");
+
+      self.$document.on('keyup', function(e) {
+        if(e.keyCode == 37)
+          self.goLeft();
+
+        if(e.keyCode == 39)
+          self.goRight();
+      });
+    },
+
+    moveSlider: function(x) {
+      var self = this;
+
+      self.$list.transition({ x: x });
+    },
+
+    goLeft: function() {
+      var self = this;
+
+      x = self.movedX + self.opts.moveDistanceInPx;
+
+      self.moveSlider(x);
+
+      self.movedX = x;
+    },
+
+    goRight: function() {
+      var self = this;
+
+      x = self.movedX - self.opts.moveDistanceInPx;
+
+      self.moveSlider(x);
+
+      self.movedX = x;
+    },
+
+    showSlider: function() {
+      var self = this;
+
+      self.$wrapper.fadeIn(1000);
+    },
+
+    enlargeList: function(callback) {
+      var self = this;
+
+      self.getDimensionsOfList(function(dimensions) {
+        var fullWidth;
+
+        fullWidth = dimensions.fullWidth;
+
+        self.$list.width(fullWidth);
+
+        self.$wrapper.hide();
+
+        callback();
+      });
+    },
+
+    getDimensionsOfList: function(callback) {
       var self = this;
 
       self.toggleElementForMeasure();
 
       self.getDimensionListElements(function(dimensions) {
-
+        self.toggleElementForMeasure();
+        callback(dimensions);
       });
-
-      self.toggleElementForMeasure();
     },
 
     toggleElementForMeasure: function() {
       var self = this;
 
-      if(self.$element.hasClass('visibleForMeasure'))
-        self.$element.removeClass('visibleForMeasure');
+      if(self.$wrapper.hasClass('visibleForMeasure'))
+        self.$wrapper.removeClass('visibleForMeasure');
       else
-        self.$element.addClass('visibleForMeasure');
+        self.$wrapper.addClass('visibleForMeasure');
     },
 
     getDimensionListElements: function(callback) {
       var self = this;
 
-      var fullWidth, numOfListElements, maxHeight, dimensions;
+      var fullWidth, numOfListElements, maxHeight, dimensions, safetyAddonPixel;
 
       numOfListElements = self.$listElements.length;
 
       fullWidth = 0;
       maxHeight = 0;
+      safetyAddonPixel = 0;
       self.$listElements.each(function(index) {
         var $listElement, listWidth, maxHeight;
 
@@ -77,13 +151,20 @@
 
         fullWidth += listWidth;
 
+        if(index+1 != numOfListElements)
+          return;
+
+        if(navigator.userAgent.match(/msie/i))
+          safetyAddonPixel = fullWidth/2000;
+        else if(navigator.userAgent.match(/mozilla/i))
+          safetyAddonPixel = fullWidth/5000;
+
         dimensions = {
           'maxHeight': maxHeight,
-          'fullWidth': fullWidth
+          'fullWidth': fullWidth + safetyAddonPixel
         };
 
-        if(index+1 == numOfListElements)
-          callback(dimensions);
+        callback(dimensions);
       });
     }
   };
